a6c48ced8bf60dd4324105b1dc3c317f58d85dff
Konstantin Wachendorff <kw@eyev.de>
2022-01-27 17:10:19 +0100

    added provider

--- lib/widgets/switch_button.dart
+++ lib/widgets/switch_button.dart
@@ -7,11 +7,24 @@
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'button.dart';
 
+class GazeSwitchButtonState extends ChangeNotifier {
+  bool _toggled;
+  bool get toggled => _toggled;
+  set toggled(bool value) {
+    if (value != _toggled) {
+      _toggled = value;
+      notifyListeners();
+    }
+  }
+
+  GazeSwitchButtonState({required bool toggled}) : _toggled = toggled;
+}
+
 class GazeSwitchButtonProperties {
-  final bool initial;
   final bool enabled;
 
   final Color disabledColor;
@@ -21,8 +34,9 @@ class GazeSwitchButtonProperties {
   final Size size;
   final EdgeInsets margin;
   final String route;
+  final GazeSwitchButtonState state;
   GazeSwitchButtonProperties({
-    required this.initial,
+    required this.state,
     this.enabled = true,
     this.disabledColor = Colors.grey,
     this.unToggledColor = Colors.grey,
@@ -49,22 +63,19 @@ class GazeSwitchButton extends StatefulWidget {
 
 class _GazeSwitchButtonState extends State<GazeSwitchButton> with SingleTickerProviderStateMixin {
   late AnimationController controller = AnimationController(duration: const Duration(milliseconds: 300), vsync: this);
-  late Animation<double> animation;
-  late bool toggled;
+  late Animation<double> animation = Tween<double>(begin: math.pi, end: math.pi / 2).animate(
+    CurvedAnimation(
+      parent: controller,
+      curve: Curves.elasticInOut,
+    ),
+  );
 
   _GazeSwitchButtonState();
 
   @override
   void initState() {
     super.initState();
-    toggled = widget.properties.initial;
-    animation = Tween<double>(begin: math.pi, end: math.pi / 2).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Curves.elasticInOut,
-      ),
-    );
-    if (toggled) {
+    if (widget.properties.state.toggled) {
       controller.forward();
     }
   }
@@ -77,61 +88,64 @@ class _GazeSwitchButtonState extends State<GazeSwitchButton> with SingleTickerPr
 
   @override
   Widget build(BuildContext context) {
-    return GazeButton(
-      properties: GazeButtonProperties(
-        key: GlobalKey(),
-        innerPadding: const EdgeInsets.all(0),
-        route: widget.properties.route,
-        child: AnimatedContainer(
-          width: widget.properties.size.width,
-          height: widget.properties.size.height,
-          duration: const Duration(milliseconds: 300),
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(
-              color: _getColor(),
-              width: 3,
-            ),
-          ),
-          child: AnimatedBuilder(
-            animation: animation,
-            builder: (context, child) => Transform.rotate(
-              angle: animation.value,
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                margin: widget.properties.margin,
-                decoration: BoxDecoration(color: _getColor(), borderRadius: BorderRadius.circular(5)),
+    return ChangeNotifierProvider.value(
+      value: widget.properties.state,
+      builder: (context, child) => Consumer<GazeSwitchButtonState>(
+        builder: (context, _state, child) => GazeButton(
+          properties: GazeButtonProperties(
+            key: GlobalKey(),
+            innerPadding: const EdgeInsets.all(0),
+            route: widget.properties.route,
+            child: AnimatedContainer(
+              width: widget.properties.size.width,
+              height: widget.properties.size.height,
+              duration: const Duration(milliseconds: 300),
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: _getColor(_state.toggled),
+                  width: 3,
+                ),
+              ),
+              child: AnimatedBuilder(
+                animation: animation,
+                builder: (context, child) => Transform.rotate(
+                  angle: animation.value,
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    margin: widget.properties.margin,
+                    decoration: BoxDecoration(color: _getColor(_state.toggled), borderRadius: BorderRadius.circular(5)),
+                  ),
+                ),
               ),
             ),
           ),
+          onTap: widget.properties.enabled
+              ? () async {
+                  _toggle(_state.toggled);
+                  if (widget.onToggled != null) {
+                    if (!await widget.onToggled!(widget.properties.state.toggled)) {
+                      _toggle(_state.toggled);
+                    }
+                  }
+                }
+              : null,
         ),
       ),
-      onTap: widget.properties.enabled
-          ? () async {
-              _toggle();
-              if (widget.onToggled != null) {
-                if (!await widget.onToggled!(toggled)) {
-                  _toggle();
-                }
-              }
-            }
-          : null,
     );
   }
 
-  void _toggle() {
+  void _toggle(bool toggled) {
     if (toggled) {
       controller.reverse();
     } else {
       controller.forward();
     }
-    setState(() {
-      toggled = !toggled;
-    });
+    widget.properties.state.toggled = !toggled;
   }
 
-  Color _getColor() {
+  Color _getColor(bool toggled) {
     return widget.properties.enabled
         ? toggled
             ? widget.properties.toggledColor
