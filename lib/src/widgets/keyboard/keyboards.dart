0b47c71c98f53739b6af2503d681f9e5f537ac17
Viviane Rehor <vr@eyev.de>
2023-07-25 18:14:45 +0200

    added onTabClose kex, refactored lang settings

--- lib/src/widgets/keyboard/keyboards.dart
+++ lib/src/widgets/keyboard/keyboards.dart
@@ -36,20 +36,108 @@ class Keyboards {
             return _english(keyboardState);
         }
       case KeyboardType.editor:
-        return _germanEditor(keyboardState);
+        switch (lang) {
+          case Language.german:
+            return _germanEditor(keyboardState);
+          case Language.english:
+            return _englishEditor(keyboardState);
+        }
       case KeyboardType.speak:
-        return _speak(keyboardState);
+        switch (lang) {
+          case Language.german:
+            return _germanSpeak(keyboardState);
+          case Language.english:
+            return _englishSpeak(keyboardState);
+        }
     }
+  }
+
+  static List<List<Widget>> _englishEditor(GazeKeyboardState keyboardState) {
+    return [
+      [
+        GazeKey(content: Icons.keyboard_tab_rounded, type: GazeKeyType.tab, keyboardState: keyboardState), // widthRatio: 1.7
+        GazeKey(content: const ['~', '`'], keyboardState: keyboardState),
+        GazeKey(content: const ['!', '1'], keyboardState: keyboardState),
+        GazeKey(content: const ['@', '2'], keyboardState: keyboardState),
+        GazeKey(content: const ['#', '3'], keyboardState: keyboardState),
+        GazeKey(content: const [r'$', '4'], keyboardState: keyboardState),
+        GazeKey(content: const ['%', '5'], keyboardState: keyboardState),
+        GazeKey(content: const ['^', '6'], keyboardState: keyboardState),
+        GazeKey(content: const ['&', '7'], keyboardState: keyboardState),
+        GazeKey(content: const ['*', '8'], keyboardState: keyboardState),
+        GazeKey(content: const ['(', '9'], keyboardState: keyboardState),
+        GazeKey(content: const [')', '0'], keyboardState: keyboardState),
+        GazeKey(content: const ['_', '-'], keyboardState: keyboardState),
+        GazeKey(content: const ['+', '='], keyboardState: keyboardState),
+        // GazeKey(content: const ['`', '´'], keyboardState: keyboardState),
+        // GazeKey(content: Icons.keyboard_backspace_rounded, widthRatio: 2.2, type: GazeKeyType.del),
+      ],
+      [
+        GazeKey(content: 'q', keyboardState: keyboardState),
+        GazeKey(content: 'w', keyboardState: keyboardState),
+        GazeKey(content: 'e', keyboardState: keyboardState),
+        GazeKey(content: 'r', keyboardState: keyboardState),
+        GazeKey(content: 't', keyboardState: keyboardState),
+        GazeKey(content: 'y', keyboardState: keyboardState),
+        GazeKey(content: 'u', keyboardState: keyboardState),
+        GazeKey(content: 'i', keyboardState: keyboardState),
+        GazeKey(content: 'o', keyboardState: keyboardState),
+        GazeKey(content: 'p', keyboardState: keyboardState),
+        GazeKey(content: const ['{', '['], keyboardState: keyboardState),
+        GazeKey(content: const ['}', ']'], keyboardState: keyboardState),
+        GazeKey(content: const ['|', r'\'], keyboardState: keyboardState),
+        GazeKey(content: 'Enter', widthRatio: 1.5, type: GazeKeyType.enter, keyboardState: keyboardState),
+      ],
+      [
+        GazeKey(content: Icons.keyboard_capslock_rounded, type: GazeKeyType.caps, keyboardState: keyboardState), // widthRatio: 2
+        GazeKey(content: 'a', keyboardState: keyboardState),
+        GazeKey(content: 's', keyboardState: keyboardState),
+        GazeKey(content: 'd', keyboardState: keyboardState),
+        GazeKey(content: 'f', keyboardState: keyboardState),
+        GazeKey(content: 'g', keyboardState: keyboardState),
+        GazeKey(content: 'h', keyboardState: keyboardState),
+        GazeKey(content: 'j', keyboardState: keyboardState),
+        GazeKey(content: 'k', keyboardState: keyboardState),
+        GazeKey(content: 'l', keyboardState: keyboardState),
+        GazeKey(content: const [':', ';'], keyboardState: keyboardState),
+        GazeKey(content: const ['"', "'"], keyboardState: keyboardState),
+        // GazeKey(content: 'Enter', widthRatio: 1.2, type: GazeKeyType.enter),
+      ],
+      [
+        GazeKey(content: Icons.keyboard_arrow_up_rounded, type: GazeKeyType.shift, keyboardState: keyboardState), // , widthRatio: 1.5
+        GazeKey(content: 'z', keyboardState: keyboardState),
+        GazeKey(content: 'x', keyboardState: keyboardState),
+        GazeKey(content: 'c', keyboardState: keyboardState),
+        GazeKey(content: 'v', keyboardState: keyboardState),
+        GazeKey(content: 'b', keyboardState: keyboardState),
+        GazeKey(content: 'n', keyboardState: keyboardState),
+        GazeKey(content: 'm', keyboardState: keyboardState),
+        GazeKey(content: const ['<', ','], keyboardState: keyboardState),
+        GazeKey(content: const ['>', '.'], keyboardState: keyboardState),
+        GazeKey(content: const ['?', '/'], keyboardState: keyboardState),
+        // GazeKey(content: 'shift', widthRatio: 1.5, shift: shift, type: GazeKeyType.shift),
+      ],
+      if (!Platform.isIOS && !Platform.isAndroid)
+        [
+          GazeKey(content: 'ctrl', widthRatio: 1.5, type: GazeKeyType.ctrl, keyboardState: keyboardState),
+          if (Platform.isWindows) GazeKey(content: 'win', widthRatio: 1.2, type: GazeKeyType.win, keyboardState: keyboardState),
+          GazeKey(content: 'alt', widthRatio: 1.5, type: GazeKeyType.alt, keyboardState: keyboardState),
+          GazeKey(content: ' ', widthRatio: 11.7, keyboardState: keyboardState),
+          GazeKey(content: 'alt gr', widthRatio: 1.5, type: GazeKeyType.alt, keyboardState: keyboardState),
+          if (Platform.isWindows) GazeKey(content: 'win', widthRatio: 1.2, type: GazeKeyType.win, keyboardState: keyboardState),
+          GazeKey(content: 'ctrl', widthRatio: 1.5, type: GazeKeyType.ctrl, keyboardState: keyboardState),
+        ]
+    ];
   }
 
   static List<List<Widget>> _germanEditor(GazeKeyboardState keyboardState) {
     return [
       [
         GazeKey(content: Icons.keyboard_tab_rounded, type: GazeKeyType.tab, keyboardState: keyboardState), // widthRatio: 1.7
-        GazeKey(content: const ['°', '**'], keyboardState: keyboardState),
+        GazeKey(content: const ['°', '^'], keyboardState: keyboardState),
         GazeKey(content: const ['!', '1'], keyboardState: keyboardState),
         GazeKey(content: const ['"', '2'], keyboardState: keyboardState),
-        GazeKey(content: const ['§', '3'], keyboardState: keyboardState),
+        GazeKey(content: const ['@', '3'], keyboardState: keyboardState),
         GazeKey(content: const [r'$', '4'], keyboardState: keyboardState),
         GazeKey(content: const ['%', '5'], keyboardState: keyboardState),
         GazeKey(content: const ['&', '6'], keyboardState: keyboardState),
@@ -58,7 +146,7 @@ class Keyboards {
         GazeKey(content: const [')', '9'], keyboardState: keyboardState),
         GazeKey(content: const ['=', '0'], keyboardState: keyboardState),
         GazeKey(content: const ['?', 'ß'], keyboardState: keyboardState),
-        // GazeKey(content: const ['`', '´'], keyboardState: keyboardState),
+        GazeKey(content: const ['`', '´'], keyboardState: keyboardState),
         // GazeKey(content: Icons.keyboard_backspace_rounded, widthRatio: 2.2, type: GazeKeyType.del),
       ],
       [
@@ -127,7 +215,7 @@ class Keyboards {
         GazeKey(content: const ['°', '^'], keyboardState: keyboardState),
         GazeKey(content: const ['!', '1'], keyboardState: keyboardState),
         GazeKey(content: const ['"', '2'], keyboardState: keyboardState),
-        GazeKey(content: const ['§', '3'], keyboardState: keyboardState),
+        GazeKey(content: const ['@', '3'], keyboardState: keyboardState),
         GazeKey(content: const [r'$', '4'], keyboardState: keyboardState),
         GazeKey(content: const ['%', '5'], keyboardState: keyboardState),
         GazeKey(content: const ['&', '6'], keyboardState: keyboardState),
@@ -194,6 +282,7 @@ class Keyboards {
         if (!Platform.isIOS && !Platform.isAndroid) GazeKey(content: 'alt gr', widthRatio: 1.5, type: GazeKeyType.alt, keyboardState: keyboardState),
         if (Platform.isWindows) GazeKey(content: 'win', widthRatio: 1.2, type: GazeKeyType.win, keyboardState: keyboardState),
         if (!Platform.isIOS && !Platform.isAndroid) GazeKey(content: 'ctrl', widthRatio: 1.5, type: GazeKeyType.ctrl, keyboardState: keyboardState),
+        GazeKey(content: Icons.keyboard_hide, widthRatio: 2, type: GazeKeyType.close, keyboardState: keyboardState),
       ]
     ];
   }
@@ -265,16 +354,17 @@ class Keyboards {
       [
         if (!Platform.isIOS && !Platform.isAndroid) GazeKey(content: 'ctrl', widthRatio: 1.5, type: GazeKeyType.ctrl, keyboardState: keyboardState),
         if (Platform.isWindows) GazeKey(content: 'win', widthRatio: 1.2, type: GazeKeyType.win, keyboardState: keyboardState),
-        GazeKey(content: 'alt', widthRatio: 1.5, type: GazeKeyType.alt, keyboardState: keyboardState),
+        if (!Platform.isIOS && !Platform.isAndroid) GazeKey(content: 'alt', widthRatio: 1.5, type: GazeKeyType.alt, keyboardState: keyboardState),
         GazeKey(content: ' ', widthRatio: 11.7, keyboardState: keyboardState),
         if (!Platform.isIOS && !Platform.isAndroid) GazeKey(content: 'alt gr', widthRatio: 1.5, type: GazeKeyType.alt, keyboardState: keyboardState),
         if (Platform.isWindows) GazeKey(content: 'win', widthRatio: 1.2, type: GazeKeyType.win, keyboardState: keyboardState),
         if (!Platform.isIOS && !Platform.isAndroid) GazeKey(content: 'ctrl', widthRatio: 1.5, type: GazeKeyType.ctrl, keyboardState: keyboardState),
+        GazeKey(content: Icons.keyboard_hide, widthRatio: 2, type: GazeKeyType.close, keyboardState: keyboardState),
       ]
     ];
   }
 
-  static List<List<Widget>> _speak(GazeKeyboardState keyboardState) {
+  static List<List<Widget>> _germanSpeak(GazeKeyboardState keyboardState) {
     return [
       [
         const Spacer(),
@@ -307,6 +397,49 @@ class Keyboards {
       [
         GazeKey(content: Icons.keyboard_arrow_up_rounded, type: GazeKeyType.shift, keyboardState: keyboardState), // , widthRatio: 1.5
         GazeKey(content: 'y', keyboardState: keyboardState),
+        GazeKey(content: 'x', keyboardState: keyboardState),
+        GazeKey(content: 'c', keyboardState: keyboardState),
+        GazeKey(content: 'v', keyboardState: keyboardState),
+        GazeKey(content: ' ', widthRatio: 2, keyboardState: keyboardState),
+        GazeKey(content: 'b', keyboardState: keyboardState),
+        GazeKey(content: 'n', keyboardState: keyboardState),
+        GazeKey(content: 'm', keyboardState: keyboardState),
+        GazeKey(content: const ['.', ','], keyboardState: keyboardState),
+        GazeKey(content: const ['?', '!'], keyboardState: keyboardState),
+      ],
+    ];
+  }
+
+  static List<List<Widget>> _englishSpeak(GazeKeyboardState keyboardState) {
+    return [
+      [
+        const Spacer(),
+        GazeKey(content: 'q', keyboardState: keyboardState),
+        GazeKey(content: 'w', keyboardState: keyboardState),
+        GazeKey(content: 'e', keyboardState: keyboardState),
+        GazeKey(content: 'r', keyboardState: keyboardState),
+        GazeKey(content: 't', keyboardState: keyboardState),
+        GazeKey(content: 'y', keyboardState: keyboardState),
+        GazeKey(content: 'u', keyboardState: keyboardState),
+        GazeKey(content: 'i', keyboardState: keyboardState),
+        GazeKey(content: 'o', keyboardState: keyboardState),
+        GazeKey(content: 'p', keyboardState: keyboardState),
+      ],
+      [
+        GazeKey(content: Icons.keyboard_capslock_rounded, type: GazeKeyType.caps, keyboardState: keyboardState),
+        GazeKey(content: 'a', keyboardState: keyboardState),
+        GazeKey(content: 's', keyboardState: keyboardState),
+        GazeKey(content: 'd', keyboardState: keyboardState),
+        GazeKey(content: 'f', keyboardState: keyboardState),
+        GazeKey(content: 'g', keyboardState: keyboardState),
+        GazeKey(content: 'h', keyboardState: keyboardState),
+        GazeKey(content: 'j', keyboardState: keyboardState),
+        GazeKey(content: 'k', keyboardState: keyboardState),
+        GazeKey(content: 'l', keyboardState: keyboardState),
+      ],
+      [
+        GazeKey(content: Icons.keyboard_arrow_up_rounded, type: GazeKeyType.shift, keyboardState: keyboardState), // , widthRatio: 1.5
+        GazeKey(content: 'z', keyboardState: keyboardState),
         GazeKey(content: 'x', keyboardState: keyboardState),
         GazeKey(content: 'c', keyboardState: keyboardState),
         GazeKey(content: 'v', keyboardState: keyboardState),
