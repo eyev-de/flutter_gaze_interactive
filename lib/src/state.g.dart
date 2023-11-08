563aec2cbfc6319b5340c00948b75bd6705551d4
Viviane Rehor <vr@eyev.de>
2023-11-08 13:32:50 +0100

    added generated files

---
+++ lib/src/state.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$snapActiveStateHash() => r'182e05e30881f9225b4ca1525e7af5ad641b6d3c';
+
+/// See also [SnapActiveState].
+@ProviderFor(SnapActiveState)
+final snapActiveStateProvider =
+    AutoDisposeNotifierProvider<SnapActiveState, bool>.internal(
+  SnapActiveState.new,
+  name: r'snapActiveStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$snapActiveStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SnapActiveState = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
