1db625f3d4d9929fb60494530b16c2f0c2df1f64
Viviane Rehor <vr@eyev.de>
2023-11-29 19:21:41 +0100

    added snap icon provider

--- lib/src/state.g.dart
+++ lib/src/state.g.dart
@@ -22,5 +22,20 @@ final snapActiveStateProvider =
 );
 
 typedef _$SnapActiveState = Notifier<bool>;
+String _$snapIconStateHash() => r'9beed2dc0f83941f0875b87008af21cfe0f35ee0';
+
+/// See also [SnapIconState].
+@ProviderFor(SnapIconState)
+final snapIconStateProvider = NotifierProvider<SnapIconState, bool>.internal(
+  SnapIconState.new,
+  name: r'snapIconStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$snapIconStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SnapIconState = Notifier<bool>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
